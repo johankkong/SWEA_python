acb77537ef3ff863dfd3c2f8a2fde5d935030694
johankkong <johankkong@gmail.com>
2022-05-22 21:41:18 +0900

    main

--- lv1/2050.py
+++ lv1/2050.py
@@ -1,5 +1,5 @@
 # 알파벳들을 string type으로 입력받는다.
 alphabets = input()
 # 알파벳들을 아스키코드를 통해 숫자로 변환한다.
-for i in alphabets:
-    print(ord(i)-64, end=" ")
+for word in alphabets:
+    print(ord(word)-64, end=" ")
